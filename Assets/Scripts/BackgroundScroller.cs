3c20b6fd1f5a3e25b698d8e31d5bc5228e75be02
Anthony Pinto <AnthonyPinto@users.noreply.github.com>
2022-09-02 10:57:33 -0700

    Add multi tier support to BG scroller

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -6,32 +6,59 @@ public class BackgroundScroller : MonoBehaviour
 {
     public SpriteRenderer tier1Sprite;
     public float tier1Speed = 15;
-    Vector3 tier1OriginalPos;
-    SpriteRenderer tier1SpriteClone;
-    float tier1SpriteWidth;
+
+    public SpriteRenderer tier2Sprite;
+    public float tier2Speed = 10;
+
+    List<BackgroundTier> backgroundTiers = new List<BackgroundTier>();
 
 
     private void Start()
     {
-        tier1SpriteWidth = tier1Sprite.bounds.size.x;
-        tier1OriginalPos = tier1Sprite.transform.position;
-        // Create a second copy of the sprite and offset it to the right by the width of the sprite we will maintain this offset
-        tier1SpriteClone = Instantiate(tier1Sprite, tier1OriginalPos + Vector3.right * tier1SpriteWidth, tier1Sprite.transform.rotation);
+        backgroundTiers.Add(new BackgroundTier(tier1Sprite, tier1Speed));
+        backgroundTiers.Add(new BackgroundTier(tier2Sprite, tier2Speed));
     }
 
     private void Update()
     {
-        Vector3 delta = Vector3.left * tier1Speed * Time.deltaTime;
-        Vector3 newPos = tier1Sprite.transform.position + delta;
-
-        // If the sprite is about to move past it's full width to the left 'wrap' the sprite back to it's start position
-        if (newPos.x < -tier1SpriteWidth)
+        foreach (BackgroundTier tier in backgroundTiers)
         {
-            newPos.x = newPos.x + tier1SpriteWidth;
+            tier.UpdateTier(Time.deltaTime);
+        }
+    }
+
+    class BackgroundTier
+    {
+        SpriteRenderer baseSprite;
+        SpriteRenderer cloneSprite;
+        float speed;
+        Vector3 originalPos;
+        float spriteWidth;
+
+        public BackgroundTier(SpriteRenderer tierSprite, float tierSpeed)
+        {
+            baseSprite = tierSprite;
+            speed = tierSpeed;
+            spriteWidth = baseSprite.bounds.size.x;
+            originalPos = baseSprite.transform.position;
+            // Create a second copy of the sprite and offset it to the right by the width of the sprite we will maintain this offset
+            cloneSprite = Instantiate(baseSprite, originalPos + Vector3.right * spriteWidth, baseSprite.transform.rotation);
         }
 
-        // Move the bg sprite to it's new pos move the clone to the updated pos plus offset
-        tier1Sprite.transform.position = newPos;
-        tier1SpriteClone.transform.position = newPos + Vector3.right * tier1SpriteWidth;
+        public void UpdateTier(float deltaTime)
+        {
+            Vector3 delta = Vector3.left * speed * Time.deltaTime;
+            Vector3 newPos = baseSprite.transform.position + delta;
+
+            // If the sprite is about to move past it's full width to the left 'wrap' the sprite back to it's start position
+            if (newPos.x < -spriteWidth)
+            {
+                newPos.x = newPos.x + spriteWidth;
+            }
+
+            // Move the bg sprite to it's new pos move the clone to the updated pos plus offset
+            baseSprite.transform.position = newPos;
+            cloneSprite.transform.position = newPos + Vector3.right * spriteWidth;
+        }
     }
 }
