5716dda582e31ccfc0770882fea6bbf9fbdd4a78
Anthony Pinto <AnthonyPinto@users.noreply.github.com>
2022-09-02 15:23:51 -0700

    Add Rat

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
