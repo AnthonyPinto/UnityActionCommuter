2e36982fc5dfd1d15383acb352a143b25059fe56
Anthony Pinto <AnthonyPinto@users.noreply.github.com>
2022-09-02 20:13:17 -0700

    Add Soda Collectables

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Poolable))]
 public class Collectable : MonoBehaviour
 {
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GetComponent<Poolable>().pool.Release(gameObject);
+        }
+    }
 }
--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -5,20 +5,28 @@ using UnityEngine.Pool;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    float timeToNextRat;
+
 
     float spawnXPos = 20;
 
-
+    float timeToNextRat;
     public Poolable ratPrefab;
     ObjectPool<GameObject> ratObjectPool;
     float ratYOffset = 0.25f;
     float maxRatWait = 6;
     float minRatWait = 1;
 
+    float timeToNextSoda;
+    public Poolable sodaPrefab;
+    ObjectPool<GameObject> sodaObjectPool;
+    float sodaYOffset = 0.25f;
+    float maxSodaWait = 8;
+    float minSodaWait = 3;
+
     private void Awake()
     {
         ratObjectPool = PoolerFactory.CreatePooler(ratPrefab.gameObject);
+        sodaObjectPool = PoolerFactory.CreatePooler(sodaPrefab.gameObject);
     }
 
 
@@ -30,6 +38,7 @@ public class ObjectSpawner : MonoBehaviour
     private void Update()
     {
         timeToNextRat -= Time.deltaTime;
+        timeToNextSoda -= Time.deltaTime;
     }
 
     IEnumerator SpawningRoutine()
@@ -44,6 +53,7 @@ public class ObjectSpawner : MonoBehaviour
 
     void SpawnObjects()
     {
+        // Spawn rat
         if (timeToNextRat <= 0)
         {
             Constants.Layer channel = Random.value > 0.5 ? Constants.Layer.ChannelOne : Constants.Layer.ChannelThree;
@@ -55,6 +65,20 @@ public class ObjectSpawner : MonoBehaviour
             rat.gameObject.layer = LayerMask.NameToLayer(Constants.LayerString[channel]);
 
             timeToNextRat = Random.Range(minRatWait, maxRatWait);
+        }
+
+        // Spawn soda
+        if (timeToNextSoda <= 0)
+        {
+            Constants.Layer rail = Constants.RailList[Random.Range(0, Constants.RailList.Count)];
+            GameObject railObject = LayerHelper.instance.GetObjectForLayer(rail);
+
+            GameObject soda = sodaObjectPool.Get();
+            soda.GetComponent<Poolable>().pool = sodaObjectPool;
+            soda.transform.position = new Vector3(spawnXPos, railObject.transform.position.y + sodaYOffset, 0);
+            soda.gameObject.layer = LayerMask.NameToLayer(Constants.LayerString[rail]);
+
+            timeToNextSoda = Random.Range(minSodaWait, maxSodaWait);
         }
     }
 
