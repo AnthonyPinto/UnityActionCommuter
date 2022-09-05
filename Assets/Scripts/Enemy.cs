b509914b0dd5db82dfd4fcb2fcdba6856073e64b
Allie Polubiec <alexandra.polubiec@gmail.com>
2022-09-05 11:25:51 -0700

    add rat animation on collision

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,16 +6,18 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int pointsOnDestroy = 50;
-
-
+    public Animator animator;
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            animator.SetTrigger("Attack");
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("PlayerAttack"))
-        {
+        {            
+            animator.SetTrigger("Attack");
             GameManager.instance.AddPoints(pointsOnDestroy);
             GetComponent<Poolable>().pool.Release(gameObject);
         }
