87aa1b74a516f712899059c33351ea5b313b614c
Anthony Pinto <AnthonyPinto@users.noreply.github.com>
2022-09-05 10:43:29 -0700

    Picking up collectables gives points

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(Poolable))]
 public class Collectable : MonoBehaviour
 {
+    public int pointValue = 100;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            GameManager.instance.AddPoints(pointValue);
             GetComponent<Poolable>().pool.Release(gameObject);
         }
     }
