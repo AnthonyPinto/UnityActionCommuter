c600c015a220968c7d565c47f8f4a84450b3e935
Allie Polubiec <alexandra.polubiec@gmail.com>
2022-09-05 13:24:17 -0700

    add audio clip for Collectibles

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     public int pointValue = 100;
+    public AudioClip audioClip;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            GameManager.instance.PlaySFX(audioClip);
             GameManager.instance.AddPoints(pointValue);
             GetComponent<Poolable>().pool.Release(gameObject);
         }
