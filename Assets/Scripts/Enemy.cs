f925a15be438da0a1292fe5b5ce9492d46e84a6f
Anthony Pinto <AnthonyPinto@users.noreply.github.com>
2022-09-05 12:32:20 -0700

    Rat Hit animation fix issue with hit/attack in same frame

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,20 +6,65 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int pointsOnDestroy = 50;
+    public float WasHitAnimationDuration = 0.75f;
     public Animator animator;
-   
+
+    bool wasHit = false;
+    bool isRoutineRunning = false;
+    bool hitPlayer = false;
+    GameObject playerObject;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (isRoutineRunning)
         {
-            animator.SetTrigger("Attack");
-            Destroy(collision.gameObject);
+            return;
         }
-        else if (collision.gameObject.CompareTag("PlayerAttack"))
-        {            
-            animator.SetTrigger("Attack");
-            GameManager.instance.AddPoints(pointsOnDestroy);
-            GetComponent<Poolable>().pool.Release(gameObject);
+
+        // We handle the results of collisions in LateUpdate because
+        // OnTriggerEnter2D can be called multiple times in a single frame
+        // resulting in both the player and the rat being hit
+        if (collision.gameObject.CompareTag("PlayerAttack"))
+        {
+            wasHit = true;
         }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            hitPlayer = true;
+            playerObject = collision.gameObject;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (wasHit)
+        {
+            wasHit = false;
+            hitPlayer = false;
+            isRoutineRunning = true;
+            StartCoroutine(WasHitRoutine());
+
+        }
+        else if (hitPlayer)
+        {
+            wasHit = false;
+            hitPlayer = false;
+            isRoutineRunning = false;
+            animator.SetTrigger("Attack");
+            Destroy(playerObject);
+
+        }
+    }
+
+    IEnumerator WasHitRoutine()
+    {
+
+        GameManager.instance.AddPoints(pointsOnDestroy);
+        animator.SetTrigger("Hit");
+        yield return new WaitForSeconds(WasHitAnimationDuration);
+
+
+        GetComponent<Poolable>().pool.Release(gameObject);
+        isRoutineRunning = false;
     }
 }
