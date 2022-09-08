0bc7e08ac24c97989ae7fb8c85d3ce2306151cb8
Anthony Pinto <AnthonyPinto@users.noreply.github.com>
2022-09-07 21:11:29 -0700

    Fix Train light and audio handling

--- Assets/Scripts/TrainSequence.cs
+++ Assets/Scripts/TrainSequence.cs
@@ -5,56 +5,49 @@ using UnityEngine;
 [RequireComponent(typeof(Poolable))]
 public class TrainSequence : MonoBehaviour
 {
+    public AudioClip trainPassingClip;
+    public AudioClip trainHornClip;
+
     public GameObject trainLightPrefab;
     public GameObject trainPrefab;
 
-    AudioSource audioSource;
-    GameObject trainLight;
+    AudioSource trainAudioSource;
     GameObject train;
 
     float trainLightDuration = 1f;
     float trainDuration = 2f;
 
-    float lightYOffset = 0;
     float trainYOffset = -0.4f;
 
     private void Awake()
     {
-        audioSource = GetComponent<AudioSource>();
-    }
-
-
-    private void OnEnable()
-    {
-        if (trainLight == null)
-        {
-            trainLight = Instantiate(trainLightPrefab);
-            trainLight.SetActive(false);
-        }
         if (train == null)
         {
             train = Instantiate(trainPrefab, transform.position, trainPrefab.transform.rotation);
             train.SetActive(false);
         }
+        trainAudioSource = train.GetComponent<AudioSource>();
+    }
 
+
+    private void OnEnable()
+    {
         StartCoroutine(OnEnableSequence());
     }
 
     IEnumerator OnEnableSequence()
     {
-        audioSource.PlayDelayed(0.5f);
         // wait for the OnEnable call to resolve so that we use thisgameObjects position AFTER it is repositioned by the object spawner
         yield return new WaitForSeconds(0);
-        trainLight.SetActive(true);
-        trainLight.SetLayerRecursively(gameObject.layer);
-        trainLight.transform.position = new Vector3(trainLight.transform.position.x, transform.position.y, trainLight.transform.position.z) + Vector3.up * lightYOffset;
-
+        train.SetActive(true);
+        trainAudioSource.PlayOneShot(trainHornClip);
+        train.GetComponent<MovingObject>().enabled = false;
+        train.SetLayerRecursively(gameObject.layer);
+        train.transform.position = transform.position + Vector3.up * trainYOffset + Vector3.left * 10;
 
         yield return new WaitForSeconds(trainLightDuration);
-        trainLight.SetActive(false);
-        train.SetActive(true);
-        train.SetLayerRecursively(gameObject.layer);
-        train.transform.position = transform.position + Vector3.up * trainYOffset;
+        trainAudioSource.PlayOneShot(trainPassingClip);
+        train.GetComponent<MovingObject>().enabled = true;
 
         yield return new WaitForSeconds(trainDuration);
         train.SetActive(false);
