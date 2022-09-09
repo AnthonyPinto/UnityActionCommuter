db13742c8fc75de86c36315ab59c73f96e996905
Anthony Pinto <AnthonyPinto@users.noreply.github.com>
2022-09-09 14:50:41 -0700

    Add transition from title to game

---
+++ Assets/Scripts/Utils/SceneHelper.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneHelper
+{
+    public const int TitleSceneIndex = 1;
+    public const int GameSceneIndex = 0;
+}
