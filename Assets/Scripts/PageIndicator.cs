a954db773e2a11599cf89e7b6aeb2a045197ef1d
Allie Polubiec <alexandra.polubiec@gmail.com>
2022-10-11 12:07:09 -0700

    dim paper indicators until collected

--- Assets/Scripts/PageIndicator.cs
+++ Assets/Scripts/PageIndicator.cs
@@ -9,8 +9,16 @@ public class PageIndicator : MonoBehaviour
     public Sprite missedSprite;
     public Image overlayImage;
 
+    Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
     public void SetOverlay(bool wasGathered)
     {
+        image.color = Color.white;
         overlayImage.sprite = wasGathered ? gatheredSprite : missedSprite;
         overlayImage.color = Color.white;
     }
